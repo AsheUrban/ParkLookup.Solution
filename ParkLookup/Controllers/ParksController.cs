e411c0436b95eedb9d7a6b3c8c57ac1f6aee4291
Ashe Urban <ashe@goldentongue.com>
2022-08-19 14:15:30 -0700

    ATTEMPT Swagger.

--- ParkLookup/Controllers/ParksController.cs
+++ ParkLookup/Controllers/ParksController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParkLookup.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ParkLookup.Controllers
 {
