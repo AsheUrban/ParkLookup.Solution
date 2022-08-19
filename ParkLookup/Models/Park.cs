bd541efec68b068ec10b5fee9d18013d6d0aef96
Ashe Urban <ashe@goldentongue.com>
2022-08-19 10:46:13 -0700

    ADD controllers and project set up.

---
+++ ParkLookup/Models/Park.cs
@@ -0,0 +1,12 @@
+namespace ParkLookup.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Feature { get; set; }
+        public string Activity { get; set; }
+    }
+}
